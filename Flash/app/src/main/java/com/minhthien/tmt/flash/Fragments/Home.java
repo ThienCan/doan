7abf0dee17ddc6360ed95c61f945c6e59657e5f9
thien.tranminh <minhthientran2501@gmail.com>
2017-02-17 16:54:40 +0700

    fix

--- Flash/app/src/main/java/com/minhthien/tmt/flash/Fragments/Home.java
+++ Flash/app/src/main/java/com/minhthien/tmt/flash/Fragments/Home.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.minhthien.tmt.flash.R;
 
@@ -13,8 +14,6 @@ import com.minhthien.tmt.flash.R;
  * A simple {@link Fragment} subclass.
  */
 public class Home extends Fragment {
-
-
     public Home() {
         // Required empty public constructor
     }
@@ -26,5 +25,6 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 
 }
--- Flash/app/src/main/java/com/minhthien/tmt/flash/MainActivity.java
+++ Flash/app/src/main/java/com/minhthien/tmt/flash/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.minhthien.tmt.flash.Adapter.ViewPagerAdapter;
 import com.minhthien.tmt.flash.Fragments.DeliveryLog;
@@ -21,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);;
-        Intent intent = new Intent(getApplicationContext(),Login.class);
+        setContentView(R.layout.activity_main);
+        ;
+        Intent intent = new Intent(getApplicationContext(), Login.class);
         startActivity(intent);
 
 
@@ -30,20 +33,24 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        init();
     }
+
     private void setupViewPager(ViewPager viewPager){
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new Home(),"Home");
-        adapter.addFragment(new DeliveryLog(),"Log");
-        adapter.addFragment(new Notification(),"Notification");
-        adapter.addFragment(new Setting(),"Account");
+        adapter.addFragment(new DeliveryLog(),"Status");
+        adapter.addFragment(new Setting(),"Setting");
         viewPager.setAdapter(adapter);
+    }
+    private void init(){
+
     }
 
 }
